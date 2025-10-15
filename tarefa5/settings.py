fbecb3928e1ca84b4d105b7a2c24e6bb5cb960c4
zByels <gabrielforti11@gmail.com>
2025-10-15 12:35:13 -0300

    Remove .env

--- tarefa5/settings.py
+++ tarefa5/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / '.env')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'uploads'
@@ -22,8 +24,10 @@ MEDIA_ROOT = BASE_DIR / 'uploads'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
+import os
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^plni5&#dj#ki_ws7!pig5++y0h&tb7p2kgja%viqmdo!6wg#^'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
